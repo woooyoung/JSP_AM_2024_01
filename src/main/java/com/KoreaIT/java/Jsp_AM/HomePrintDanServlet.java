114087fda1b611f00ff22e4e7dc819da32ecd0af
woooyoung <axdsw121@gmail.com>
2024-01-22 10:09:00 +0900

    한글 테스트

--- src/main/java/com/KoreaIT/java/Jsp_AM/HomePrintDanServlet.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/HomePrintDanServlet.java
@@ -13,6 +13,7 @@ public class HomePrintDanServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
 		request.getRequestDispatcher("/jsp/home/printDan.jsp").forward(request, response);
 	}
 
