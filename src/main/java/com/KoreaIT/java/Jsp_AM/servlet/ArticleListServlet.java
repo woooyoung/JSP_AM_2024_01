3d6a1d2014539324c3641b0468b3699415352bd6
woooyoung <axdsw121@gmail.com>
2024-01-24 13:19:34 +0900

    페이지 노출 갯수 조절

--- src/main/java/com/KoreaIT/java/Jsp_AM/servlet/ArticleListServlet.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/servlet/ArticleListServlet.java
@@ -62,8 +62,11 @@ public class ArticleListServlet extends HttpServlet {
 			List<Map<String, Object>> articleRows = DBUtil.selectRows(conn, sql);
 
 			request.setAttribute("page", page);
+			request.setAttribute("totalCnt", totalCnt);
 			request.setAttribute("totalPage", totalPage);
+			request.setAttribute("itemsInAPage", itemsInAPage);
 			request.setAttribute("articleRows", articleRows);
+
 			request.getRequestDispatcher("/jsp/article/list.jsp").forward(request, response);
 
 		} catch (SQLException e) {
